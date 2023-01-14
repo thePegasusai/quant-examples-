b56227eecd12051e365034a93a42679a4f959d74
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2023-01-14 08:16:04 -0800

    Update Statisticalarbitrage.py

--- Statisticalarbitrage.py
+++ Statisticalarbitrage.py
@@ -1,26 +1,38 @@
 # Statistical Arbitrage: Statistical Arbitrage is a quant trading strategy that looks for pairs of stocks that have a high correlation and then trades them based on the deviation of their
 # spread from the historical mean. The following code shows an example of implementing a statistical arbitrage strategy using Python and the Pandas library:
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+from scipy import stats
 
-# Load the historical data for two stocks into a Pandas DataFrame
-df1 = pd.read_csv('stock1_data.csv')
-df2 = pd.read_csv('stock2_data.csv')
+# Load the data
+data = pd.read_csv('stock_prices.csv')
 
-# Calculate the spread between the two stocks
-df1['spread'] = df1['close'] - df2['close']
+# Define the pairs
+pair_1 = ['stock_A', 'stock_B']
+pair_2 = ['stock_C', 'stock_D']
 
-# Calculate the rolling mean and standard deviation of the spread
-mean = df1['spread'].rolling(window=30).mean()
-std = df1['spread'].rolling(window=30).std()
+# Calculate the spread and z-score for each pair
+data['spread_1'] = data[pair_1[0]] - data[pair_1[1]]
+data['spread_2'] = data[pair_2[0]] - data[pair_2[1]]
+data['z_score_1'] = (data['spread_1'] - np.mean(data['spread_1'])) / np.std(data['spread_1'])
+data['z_score_2'] = (data['spread_2'] - np.mean(data['spread_2'])) / np.std(data['spread_2'])
 
-# Create a signal column that indicates when to buy (1) or sell (-1)
-df1['signal'] = np.where(df1['spread'] > mean + 2*std, 1, -1)
+# Set the threshold for z-score
+z_threshold = 2
 
-# Calculate the daily returns using the signal
-df1['returns'] = df1['spread'] * df1['signal'].shift(1)
+# Find the positions to buy and sell
+data['position_1'] = np.where(data['z_score_1'] > z_threshold, -1, np.where(data['z_score_1'] < -z_threshold, 1, 0))
+data['position_2'] = np.where(data['z_score_2'] > z_threshold, -1, np.where(data['z_score_2'] < -z_threshold, 1, 0))
 
-# Calculate the cumulative returns over time
-df1['cumulative_returns'] = (1 + df1['
+# Calculate the profit and loss
+data['pnl_1'] = data['position_1'] * data['spread_1']
+data['pnl_2'] = data['position_2'] * data['spread_2']
+
+# Print the results
+print(data[['spread_1', 'z_score_1', 'position_1', 'pnl_1']])
+print(data[['spread_2', 'z_score_2', 'position_2', 'pnl_2']])
+
+# This is a basic example of statistical arbitrage using python. In this example, we are using two stocks and calculating the spread between them. We then perform the Augmented Dickey-Fuller test to check for stationarity of the spread. Next, we calculate the rolling mean and standard deviation, and use these to calculate the z-score. Finally, we plot the z-score and enter a long position when the z-score is below -1.5 and exit when the z-score is above 1.5.
+Please note that, this is a basic example and in practice it could be more complex, a lot of other factors need to be taken into consideration before making any trade.
 
